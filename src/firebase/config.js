022cd7626f0c2b12d114a7b62b92a836355b1432
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-07-29 03:43:38 +0000

    scaffolding new firebase react project

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD6vMxRpK_HKU-WWN2gC5rXrWlrVyb-dO4",
+    authDomain: "otpproject-7714c.firebaseapp.com",
+    projectId: "otpproject-7714c",
+    storageBucket: "otpproject-7714c.appspot.com",
+    messagingSenderId: "636748451600",
+    appId: "1:636748451600:web:b15c864655d8c1646d1f95"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  
